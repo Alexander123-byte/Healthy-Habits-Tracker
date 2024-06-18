1f96f121a741a12a0cc289dfa83b696d95163f8c
Alexander <san.juckov2017@yandex.ru>
2024-06-18 11:42:28 +0300

    Access rights

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+from habits.models import Habit
+from habits.paginators import HabitPaginator
+from habits.serializers import HabitSerializer
+from users.permissions import IsOwner
+
+
+class HabitCreateAPIView(CreateAPIView):
+    """Контроллер создания привычки."""
+    serializer_class = HabitSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class HabitListAPIView(ListAPIView):
+    """Контроллер для отображения списка всех привычек."""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    pagination_class = HabitPaginator
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
+
+
+class HabitRetrieveAPIView(RetrieveAPIView):
+    """Контроллер для отображения конкретной привычки."""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsOwner,)
+
+
+class HabitUpdateAPIView(UpdateAPIView):
+    """Контроллер для обновления привычки."""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = (IsOwner,)
+
+
+class HabitDestroyAPIView(DestroyAPIView):
+    """Контроллер для удаления привычки."""
+    queryset = Habit.objects.all()
+    permission_classes = (IsOwner,)
+
+
+class PublicHabitListAPIView(ListAPIView):
+    """Контроллер для отображения всех публичных привычек."""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.filter(is_public=True)
