9d2b8d99e94fb1592c3a4e102f3a0dd05d409a97
Alexander <san.juckov2017@yandex.ru>
2024-06-18 12:02:26 +0300

    Endpoints, integration, security and documentation

---
+++ habits/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from habits.apps import HabitsConfig
+from habits.views import (HabitCreateAPIView, HabitListAPIView, PublicHabitListAPIView, HabitRetrieveAPIView,
+                          HabitUpdateAPIView, HabitDestroyAPIView)
+
+app_name = HabitsConfig.name
+
+
+urlpatterns = [
+    path('', HabitListAPIView.as_view(), name='habit_list'),
+    path('create/', HabitCreateAPIView.as_view(), name='habit_create'),
+    path('<int:pk>/', HabitRetrieveAPIView.as_view(), name='habit_detail'),
+    path('update/<int:pk>/', HabitUpdateAPIView.as_view(), name='habit_update'),
+    path('delete/<int:pk>/', HabitDestroyAPIView.as_view(), name='habit_delete'),
+    path('public/', PublicHabitListAPIView.as_view(), name='public_habits'),
+]
